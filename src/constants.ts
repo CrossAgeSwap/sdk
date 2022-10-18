f7596dbec069e7eed4867c7305ab3268b7ad7947
Blockchain Developer <55833057+bin0316@users.noreply.github.com>
2022-10-19 01:41:54 +0800

    BSC Testnet

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  TESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x968ffe31c1f27c96e27b9223250690ecc9847c3c'
+
+export const INIT_CODE_HASH = '0xa08947ba3a688357eb8845e77f2ecef8d3094a508ed53023faac7b82bc70b454'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(9975)
+export const FEES_DENOMINATOR = JSBI.BigInt(10000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
